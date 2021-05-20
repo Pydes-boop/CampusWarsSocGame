efde7a603b8f86b84338a11e011231e368ae2300
flixabua <ge38xes@mytum.de>
2021-05-20 14:01:15 +0200

    Added some requests

--- CampusWarsBackend/apis/v1/routes.py
+++ CampusWarsBackend/apis/v1/routes.py
@@ -7,9 +7,11 @@ __version__ = "0.0.1"
 
 __all__ = ('v1',)
 
-from flask import jsonify
+from flask import jsonify, request
 from flask_restful import Resource
 from apis.v1 import v1, api
+import databaseInterface
+import groupCreation
 
 
 @api.resource('/lecturehalls/<int:number>')
@@ -40,6 +42,47 @@ class Quiz(Resource):
                 {'name': 'MI-2', 'location': [51, 10]}
             ]
         )
+
+
+@api.resource('/roomdetection')
+class RoomDetection(Resource):
+    def post(self):
+        return jsonify(databaseInterface.room_detection(request.headers["latitude"], request.headers["longitude"]))
+
+    def get(self):
+        return jsonify(databaseInterface.get_all_rooms())
+
+
+@api.resource('/echo')
+class Echo(Resource):
+    def get(self):
+        return "Hallo Echo!", 200
+
+
+@api.resource('/groups')
+class Groups(Resource):
+    def get(self):
+        # todo: frontend pls define what to return
+        return jsonify(databaseInterface.get_all_groups())
+
+    def post(self):
+        # todo: get user selected groups from body not header
+        return jsonify(databaseInterface.set_user_groups(request.headers["user"], request.headers["groups"]))
+
+
+@api.resource('/start')
+class Start(Resource):
+    def post(self):
+        groupCreation.create_groups()
+        return "ok", 200
+
+
+@api.resource('/question')
+class Question(Resource):
+    def post(self):
+        # todo: get questions from body and process to database
+        databaseInterface.add_question(1, 2, 3)
+        return "ok", 200
 
 
 if __name__ == '__main__':
