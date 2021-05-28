19a93e9784c80af80f792e42dcca079aae2f8674
Daniel Klitzner <daniel.klitzner@tum.de>
2021-05-28 16:46:55 +0200

    Added some new HTTP classes

---
+++ CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/communication/HttpHeader.java
@@ -0,0 +1,37 @@
+package com.socgame.campuswars_app.communication;
+
+import java.util.HashMap;
+
+public class HttpHeader {
+
+    private HashMap<String, String> header;
+
+    public HttpHeader(){
+        header = new HashMap<String, String>();
+    }
+
+    public HashMap<String, String> getHeaders() {
+        return header;
+    }
+
+    public void addUniqueId(){
+        //TODO -> is this necessary? Should this be private?
+    }
+
+    public void buildRoomFinderHeader(double latitude, double longitude){
+        header.put("latitude", Double.toString(latitude));
+        header.put("longitude", Double.toString(longitude));
+    }
+
+    public void buildGroupsHeader(){
+        //Use addUniqueID here?
+    }
+
+    public void buildStartHeader(){
+        //TODO & discuss with backend
+    }
+
+    public void buildQuestionHeader(){
+        //TODO & Discuss with backend
+    }
+}
