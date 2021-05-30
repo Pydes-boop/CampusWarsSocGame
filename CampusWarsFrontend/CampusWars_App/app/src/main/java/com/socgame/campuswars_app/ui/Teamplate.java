d8a682aef1d0ffebaa5903b765e237aeb67eb886
Jonas <i.jonas.hack.i@googlemail.com>
2021-05-30 18:12:50 +0200

    started with the fragments (other screens)

--- CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/Teamplate.java
+++ CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/Teamplate.java
@@ -25,4 +25,32 @@ public class Teamplate extends Activity//AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teamplate);
     }
+
+    //Took this from the tab template
+    //TODO: adapt to fit here
+    /*
+    private ActivityDeleteThisBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityDeleteThisBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }*/
 }
