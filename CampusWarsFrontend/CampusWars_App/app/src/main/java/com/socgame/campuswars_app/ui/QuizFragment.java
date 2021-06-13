7ec4a058270dd908e1d54c62094b0bf37b8bf309
Pydes-boop <59734957+Pydes-boop@users.noreply.github.com>
2021-06-13 17:08:39 +0200

    Added Fragments and TabLayout to MainActivity

--- CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/QuizFragment.java
+++ CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/QuizFragment.java
@@ -2,12 +2,16 @@ package com.socgame.campuswars_app.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.socgame.campuswars_app.R;
 
 /**
@@ -62,5 +66,14 @@ public class QuizFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        /*TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText("OBJECT " + (position + 1))
+        ).attach();*/
     }
 }
