e193c49f4bc6e8682860d40a5cc651d12c8ce8e9
Jonas <i.jonas.hack.i@googlemail.com>
2021-06-20 22:54:56 +0200

    Started restructure of quiz

---
+++ CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/QuizActivity.java
@@ -0,0 +1,26 @@
+package com.socgame.campuswars_app.ui;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.socgame.campuswars_app.R;
+import com.socgame.campuswars_app.databinding.ActivityQuizBinding;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
