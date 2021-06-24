f31b9cca6c0ff80b68e6615400c01cdbfd00592c
Daniel Klitzner <daniel.klitzner@tum.de>
2021-06-24 10:37:58 +0200

    Fixed Naming in Firebase, removed services-json from gitignore because this is private repository

--- CampusWarsFrontend/CampusWars_App/app/src/androidTest/java/com/socgame/campuswars_app/ExampleInstrumentedTest.java
+++ CampusWarsFrontend/CampusWars_App/app/src/androidTest/java/com/socgame/campuswars_app/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.campuswars_app", appContext.getPackageName());
+        assertEquals("com.socgame.campuswars_app", appContext.getPackageName());
     }
 }
