92b553a488207d3e459a350f4d6155b126471d62
Pydes-boop <59734957+Pydes-boop@users.noreply.github.com>
2021-06-28 10:45:55 +0200

    Added logout button, changed some dp to sp

--- CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/TeamFragment.java
+++ CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/TeamFragment.java
@@ -1,6 +1,8 @@
 package com.socgame.campuswars_app.ui;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,9 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.socgame.campuswars_app.R;
+import com.socgame.campuswars_app.communication.FirebaseCom;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,9 +42,14 @@ public class TeamFragment extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Context ctx = this.getContext();
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_team, container, false);
         ListView listView = view.findViewById(R.id.memberList);
+
+        Button logout = (Button) view.findViewById(R.id.logoutButton);
+        FirebaseCom fCom = FirebaseCom.getInstance(ctx);
 
         //TODO: get correct info from server
         //only possible when the calls exist
@@ -61,6 +71,20 @@ public class TeamFragment extends Fragment
         //ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_activated_1, array);
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getContext(), R.layout.teammember, array);
         listView.setAdapter(itemsAdapter);
+
+        //Logout Button
+        logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                fCom.getMAuth().signOut();
+                SharedPreferences settings = ctx.getSharedPreferences("userdata", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("loggedIn", false);
+                editor.apply();
+
+                Intent myIntent = new Intent(view.getContext(), LoginActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
 
         return view;
     }
