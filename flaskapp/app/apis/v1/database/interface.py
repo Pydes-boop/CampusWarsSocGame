5b1ac685cdd9169f420f290753f176caa818c44a
Marina Weber <marina.weber@tum.de>
2021-07-01 12:49:44 +0200

    test 4

--- flaskapp/app/apis/v1/database/interface.py
+++ flaskapp/app/apis/v1/database/interface.py
@@ -107,14 +107,9 @@ def get_current_quizzes(room_id):
                                                                             "end": {"$gte": current_time[0]},
                                                                             "day": current_time[1]}}},
                                               {"_id": 1})
-    indices_quizzes = list(mongo.db.quiz.find({"lectureID": index_lecture}))
-    if not indices_quizzes:
-        room_info = mongo.db.room.find_one({"_id": room_id}, {"_id": 0})
-        return room_info
-        # indices_quizzes = list(mongo.db.quiz.find({"campusID": room_info["campusID"]}))
-
-    return list(mongo.db.quiz.find({"_id": {"$in": indices_quizzes}}))
-
+    room_info = mongo.db.room.find_one({"_id": room_id}, {"_id": 0})
+    return room_info
+   
 
 def add_quiz(name, created_by, lecture_id):
     if isinstance(lecture_id, str):
