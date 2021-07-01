ea1624be314fcf48e739e06db9034fb75d52c86d
Robin 'r0w' Weiland <robinweiland@gmx.de>
2021-07-01 19:16:55 +0200

    quiz question update

--- flaskapp/app/apis/v1/database/live_data.py
+++ flaskapp/app/apis/v1/database/live_data.py
@@ -211,7 +211,11 @@ class QuizQueue(RoomQueue):
             if uid in self and room == self[uid].room == room:  # player adds another into a game
                 opp = self.get_opponent(self[uid])
                 if opp:
-                    game = Game(game_id(), players=[self[uid], opp], question=choice(get_questions_of_quiz(choice(get_current_quizzes(ObjectId(lid))))))  # TODO
+                    game = Game(game_id=game_id(),
+                                players=[self[uid], opp],
+                                question=choice(get_current_quizzes(ObjectId(lid))[0]["_id"])
+                                # question=choice(get_questions_of_quiz(choice(get_current_quizzes(ObjectId(lid)))))
+                    )  # TODO
                     del self[uid], self[opp.uid]
                     self.game_queue[game.game_id] = game
                     return 'game-incomplete', game
