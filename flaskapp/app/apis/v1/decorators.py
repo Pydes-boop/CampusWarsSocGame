006cbaf007803f404c2c72426156a5872cb92a02
Robin 'r0w' Weiland <robinweiland@gmx.de>
2021-07-01 21:59:30 +0200

    added timed out player feature

--- flaskapp/app/apis/v1/decorators.py
+++ flaskapp/app/apis/v1/decorators.py
@@ -7,8 +7,9 @@ __version__ = "0.0.1"
 
 __all__ = ('request_requires',)
 
-from flask import request
+from flask import request, jsonify
 from functools import wraps
+from datetime import datetime
 
 from typing import Any, Callable
 
@@ -21,6 +22,17 @@ def request_requires(**dec_kwargs) -> Callable:
                 for name in map(lambda x: x.lower(), names):
                     if name not in map(lambda x: x.lower(), getattr(request, section).keys()):
                         raise Exception(f'Request was missing the {section}.{name} entry;')
+            return method(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def check_timed_out_users(timedoutusers) -> Callable:
+    def decorator(method: Callable) -> Callable:
+        @wraps(method)
+        def wrapper(*args, **kwargs) -> Any:
+            if 'uid' in request.headers and request.headers['uid'] in timedoutusers:
+                return jsonify(f'You are banned until {datetime.fromtimestamp(timedoutusers[request.headers["uid"]]).strftime("%A %d-%m-%Y, %H:%M:%S")}')
             return method(*args, **kwargs)
         return wrapper
     return decorator
