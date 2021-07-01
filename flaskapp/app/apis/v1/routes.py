3e6b4e0021c3848a7f921098c0a9790662981934
Robin 'r0w' Weiland <robinweiland@gmx.de>
2021-07-01 17:23:21 +0200

    object ids are even better part 3

--- flaskapp/app/apis/v1/routes.py
+++ flaskapp/app/apis/v1/routes.py
@@ -115,10 +115,11 @@ class QuizRefresh(Resource):
                 {
                     'gid': game.game_id,  # game_id: a 24 byte string to identify each game
                     'pid': game.get_player_id(request.headers['uid']),  # player_id: 0 or 1 identifies player in game
-                    'opp-name': 'It was you all along',  # name of the opponent TODO ask Marina how to get the name
+                    'opp-name': get_player_name(request.headers['uid']),
                     'opp-team': game.players[not game.get_player_id(request.headers['uid'])].team,
                     # name of the opponent team
-                    'quiz': game.question,
+                    # 'quiz': game.question,
+                    'quiz': dict((key, value if not isinstance(value, ObjectId) else str(value)) for key, value in game.question.items())
                     # quiz in the already specified format TODO is there a way to get just a random quiz
                     'game-ready': descriptor == 'game'  # unimportant
                 }
