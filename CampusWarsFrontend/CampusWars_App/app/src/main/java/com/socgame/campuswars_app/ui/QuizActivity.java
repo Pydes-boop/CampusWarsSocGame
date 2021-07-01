52fe0f79a20a158a4a2934ae00fa04325063c2f3
Jonas <i.jonas.hack.i@googlemail.com>
2021-07-01 12:27:21 +0200

    Implemented Quiz
    Still cant test cause of new build problems

--- CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/QuizActivity.java
+++ CampusWarsFrontend/CampusWars_App/app/src/main/java/com/socgame/campuswars_app/ui/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.socgame.campuswars_app.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -23,97 +24,151 @@ import com.socgame.campuswars_app.databinding.ActivityQuizBinding;
 
 import org.w3c.dom.Text;
 
-public class QuizActivity extends AppCompatActivity implements View.OnClickListener
-{
-    /**
-     * Gets Data from calling Activity via Bundle
-     * creates Quiz Duel between 2 Players
-     *
-     * sends Data back to Server
-     *
-     * written by Jonas and Daniel
-     */
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * Gets Data from calling Activity via Bundle
+ * creates Quiz Duel between 2 Players
+ *
+ * sends Data back to Server
+ *
+ * written by Jonas and Daniel
+ */
+public class QuizActivity extends AppCompatActivity //implements View.OnClickListener
+{
     private Context ctx;
-    private int gid;
-    private int pid;
+
+    private int gameId;
+    private int playerId; //0/1
+
+    //cant display this yet
     private String oppName;
     private String oppTeam;
+
     private String question;
-    private String[] wrongAnswers;
+    private String[] wrongAnswers;//3 lang
     private String correctAnswer;
+
+    private int indexRight = -1;
+
+    //UI
+    Button buttons[] = new Button[4];
 
     //TODO Link to Server
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         ctx = this.getApplicationContext();
+
+        /*
+        //TODO: get proper info from server
         //Getting Data From Call
         Bundle b = getIntent().getExtras();
         //TODO Add topic Title, ADD challenger to Quiz Activity xml
         //TODO We get right answer now
-        if(b != null){
-            /*
-            this.question = b.getString("question");
-            this.answers = b.getStringArray("answers");
-            this.correctAnswer = b.getInt("correctAnswer");
-            this.challenger = b.getString("challenger");
-            */
+        if(b != null)
+        {
+
+            //this.question = b.getString("question");
+            //this.answers = b.getStringArray("answers");
+            //this.correctAnswer = b.getInt("correctAnswer");
+            //this.challenger = b.getString("challenger");
+
             //TODO FIXME FOR NEW HTTP CALL
             //MAYBE ADD CORRECT ANSWER TO ARRAY of WRONG ANSWERS AND THEN RANDOMIZE?
         }
+        */
 
-        TextView textQuestion = (TextView) findViewById(R.id.questionText);
-        TextView textTitle = (TextView) findViewById(R.id.topicText);
+        debugValues();
 
-        Button option1 = (Button) findViewById(R.id.answerButtonA);
-        Button option2 = (Button) findViewById(R.id.answerButtonB);
-        Button option3 = (Button) findViewById(R.id.answerButtonC);
-        Button option4 = (Button) findViewById(R.id.answerButtonD);
-
-        textQuestion.setText(this.question);
-        textTitle.setText(this.challenger); //TODO change this to topic Title and add Challenger textView
-
-        option1.setText(this.answers[0]);
-        option2.setText(this.answers[1]);
-        option3.setText(this.answers[2]);
-        option4.setText(this.answers[3]);
-
-        option1.setOnClickListener(this);
-        option2.setOnClickListener(this);
-        option3.setOnClickListener(this);
-        option4.setOnClickListener(this);
+        setUI();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.answerButtonA:
-                checkAnswer(1);
-                break;
-            case R.id.answerButtonB:
-                checkAnswer(2);
-                break;
-            case R.id.answerButtonC:
-                checkAnswer(3);
-                break;
-            case R.id.answerButtonD:
-                checkAnswer(4);
-                break;
-            default:
-                break;
+    //TODO: this can be deleted once the http call is inplace
+    private void debugValues()
+    {
+        gameId = 0;
+        playerId = 0; //0/1
+
+        //cant display this yet
+        oppName = "test enemy";
+        oppTeam = "evil team";
+
+        question = "What is the average air speed velocity of a laden swallow?";
+        String[] temp = {"40 m/s", "Faster than an ant, slower than a bee", "Wouldnt you like to know, weatherboy?"};
+        wrongAnswers = temp;//3 lang
+        correctAnswer = "What do you mean, an African or European Swallow?";
+    }
+
+
+    private void setUI()
+    {
+        //Set UI text
+
+        //TODO: get topic text
+        TextView topicText = this.findViewById(R.id.topicText);
+        topicText.setText("TODO: get real topic");
+
+        //Also what do I do with the opposing team/player name?
+
+
+        //Randomize answer order
+        List<String> allAnswers = Arrays.asList(wrongAnswers);
+        allAnswers.add(correctAnswer);
+        Collections.shuffle(allAnswers);
+        indexRight = allAnswers.indexOf(correctAnswer);
+
+
+        //give answers to buttons
+        buttons[0] = this.findViewById(R.id.answerButtonA);
+        buttons[1] = this.findViewById(R.id.answerButtonB);
+        buttons[2] = this.findViewById(R.id.answerButtonC);
+        buttons[3] = this.findViewById(R.id.answerButtonD);
+
+        for(int i = 0; i < buttons.length; i++)
+        {
+            buttons[i].setText(allAnswers.get(i));
         }
+
+
+        //set responses
+        buttons[0].setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){ answer(0);}
+        });
+
+        buttons[1].setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){ answer(1);}
+        });
+
+        buttons[2].setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){ answer(2);}
+        });
+
+        buttons[3].setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){ answer(3);}
+        });
     }
 
-    private void checkAnswer(int chosenAnswer){
-        //TODO fix check
-        /*
-        if(chosenAnswer == this.correctAnswer){
-            Toast.makeText(this.ctx, "Correct Answer, Congrats!", Toast.LENGTH_SHORT).show();
-        } else{
-            Toast.makeText(this.ctx, "Wrong Answer, Sad :(", Toast.LENGTH_SHORT).show();
-        }*/
+    private void answer(int buttonIndex)
+    {
+        //TODO: send answer to server
+
+        if(buttonIndex == indexRight) //Correct Answer
+        {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_LONG).show();
+        }
+        else //wrong answer
+        {
+            Toast.makeText(this, "False", Toast.LENGTH_LONG).show();
+        }
     }
 }
