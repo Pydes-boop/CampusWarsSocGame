9e9e0174d4b0ba569412246fe3b0063d4cb7b808
Robin 'r0w' Weiland <robinweiland@gmx.de>
2021-07-05 22:40:12 +0200

    down with the bots

--- flaskapp/app/app.py
+++ flaskapp/app/app.py
@@ -9,9 +9,26 @@ __all__ = ('create_app',)
 
 from os import environ
 from config import DevelopmentConfig
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from random import choice
 from apis.v1.routes import v1
 from apis.v1 import mongo
+
+
+def check_ua(ua, candidates) -> bool:
+    for uac in candidates:
+        if uac in ua: return True
+    else:
+        return False
+
+
+STATUS = [
+    204, 205, 206,
+    300, 301, 302, 303, 304, 305, 307, 308,
+    400, 402, 404, 405, 406, 407, 408, 409,
+    410, 411, 412, 413, 414, 415, 416, 417,
+    418, 425, 426, 428, 429, 431, 451
+]
 
 
 def create_app():
@@ -24,6 +41,13 @@ def create_app():
                               + environ['MONGODB_PASSWORD'] + '@' \
                               + environ['MONGODB_HOSTNAME'] + ':27017/' + environ['MONGODB_DATABASE']
     mongo.init_app(app)
+
+    @app.before_request
+    def deny():
+        if 'live-debug' not in request.url_rule and not check_ua(
+                request.headers.get('User-Agent'), ['CampusWarsFrontend', 'Postman', 'Android']
+        ):
+            abort(418)
 
     @app.after_request
     def header(response):
